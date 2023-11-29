8a223cbc8ee8011ef0701872fff70c935bc30cb9
Kseniya <ksuta_marfa@mil.ru>
2023-11-29 12:21:17 +0300

    conflict

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { ForApp } from "./ForApp";
 import { Login } from "./components/Login/Login";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/header";
+import { MainContent } from "./components/MainContent/MainContent";
+import { getDeleprices } from "../src/utils/index";
+import { useEffect } from "react";
 
 function App() {
   return (
