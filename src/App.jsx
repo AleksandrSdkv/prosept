8cf2b315454fb42a0aa59363ec6642b722c3adad
Aleksandr Sadykov <Aleksandr2697@yandex.ru>
2023-12-03 15:07:53 +0300

    dilers table

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { ForApp } from "./ForApp";
 import { Login } from "./components/Login/Login";
-import { Footer } from "./components/Footer/Footer";
-import { Header } from "./components/Header/header";
-import { MainContent } from "./components/MainContent/MainContent";
-import { getDeleprices } from "../src/utils/index";
-import { useEffect } from "react";
 
 function App() {
-  login();
   return (
     <>
       <Routes>
-        <Route
-          path='/'
-          element={
-            <ForApp />
-          }
-        ></Route>
-        <Route
-          path='/signin'
-          element={
-          <Login />
-          }
-        ></Route>
+        <Route path="/" element={<ForApp />}></Route>
+        <Route path="/signin" element={<Login />}></Route>
       </Routes>
     </>
   );
